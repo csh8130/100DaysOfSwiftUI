320cbc6836e20f58d23c86230b94774ff2878421
csh8130 <csh8130@naver.com>
2020-07-05 00:25:54 +0900

    day16 init

---
+++ day16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Choi SeungHyuk on 2020/07/05.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
