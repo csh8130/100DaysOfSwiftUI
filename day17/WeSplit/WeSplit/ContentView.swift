78ee344d0b1b88e4a021f14d5597c49769e69beb
csh8130 <csh8130@naver.com>
2020-07-06 23:25:58 +0900

    계산식 추가, 계산결과 출력

--- day17/WeSplit/WeSplit/ContentView.swift
+++ day17/WeSplit/WeSplit/ContentView.swift
@@ -15,6 +15,18 @@ struct ContentView: View {
     @State private var tipPercentage = 2 //index
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -39,7 +51,7 @@ struct ContentView: View {
                 .pickerStyle(SegmentedPickerStyle())
                 
                 Section {
-                    Text("$\(checkAmount)")
+                    Text("$\(totalPerPerson)")
                 }
             }
             .navigationBarTitle("WeSplit")
