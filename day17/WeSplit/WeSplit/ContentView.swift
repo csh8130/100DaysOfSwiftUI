250fa1532a84e298310b3a85c26c4916abc48a89
csh8130 <csh8130@naver.com>
2020-07-06 22:53:15 +0900

    day17 init

---
+++ day17/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Choi SeungHyuk on 2020/07/05.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    
+    @State private var tipPercentage = 2 //index
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+            }
+            
+            Section {
+                Text("$\(checkAmount)")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
