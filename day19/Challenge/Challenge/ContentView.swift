0c6b7b516c9469a6e476e548c5d71190c9313f58
csh8130 <csh8130@naver.com>
2020-07-09 00:00:01 +0900

    유저로부터 길이 입력받기위한 코드

--- day19/Challenge/Challenge/ContentView.swift
+++ day19/Challenge/Challenge/ContentView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userInput = ""
+    @State private var userInputType = 0
+    let inputTypes = ["m", "km", "ft", "yd"]
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("변환하고 싶은 길이 입력")) {
+                    TextField("길이", text: $userInput)
+                        .keyboardType(.decimalPad)
+                    Picker("입력 단위", selection: $userInputType) {
+                        ForEach(0 ..< inputTypes.count) {
+                            Text("\(self.inputTypes[$0])")
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("길이 변환")
+        }
     }
 }
 
