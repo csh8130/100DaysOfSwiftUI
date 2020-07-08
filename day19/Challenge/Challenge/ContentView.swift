a6e486867373a25f7a87fc283991d4a9f424f187
csh8130 <csh8130@naver.com>
2020-07-09 00:29:34 +0900

    길이 변환 결과 출력

--- day19/Challenge/Challenge/ContentView.swift
+++ day19/Challenge/Challenge/ContentView.swift
@@ -11,7 +11,40 @@ import SwiftUI
 struct ContentView: View {
     @State private var userInput = ""
     @State private var userInputType = 0
-    let inputTypes = ["m", "km", "ft", "yd"]
+    @State private var outputType = 0
+    let types = ["m", "km", "ft", "yd"]
+    
+    var output: Double {
+        let userDouble = Double(userInput) ?? 0
+        var userMeter: Double
+        
+        switch userInputType {
+        case 0:
+            userMeter = userDouble
+        case 1:
+            userMeter = userDouble * 1000
+        case 2:
+            userMeter = userDouble / 3.28084
+        case 3:
+            userMeter = userDouble / 1.09361
+        default:
+            userMeter = -1
+        }
+        
+        switch outputType {
+            case 0:
+                return userMeter
+            case 1:
+                return userMeter / 1000
+            case 2:
+                return userMeter * 3.28084
+            case 3:
+                return userMeter * 1.09361
+            default:
+                return -1
+        }
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -19,10 +52,21 @@ struct ContentView: View {
                     TextField("길이", text: $userInput)
                         .keyboardType(.decimalPad)
                     Picker("입력 단위", selection: $userInputType) {
-                        ForEach(0 ..< inputTypes.count) {
-                            Text("\(self.inputTypes[$0])")
+                        ForEach(0 ..< types.count) {
+                            Text("\(self.types[$0])")
                         }
                     }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("변환 결과")) {
+                    Text("\(output, specifier: "%.2f") \(types[outputType])")
+                    Picker("변환 단위", selection: $outputType) {
+                        ForEach(0 ..< types.count) {
+                            Text("\(self.types[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
             }
             .navigationBarTitle("길이 변환")
