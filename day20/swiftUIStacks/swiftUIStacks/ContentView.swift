e448fd665ecdfb7eb36f4793807875b27aace4dc
csh8130 <csh8130@naver.com>
2020-07-10 01:16:40 +0900

    day20 swiftUI stacks init

---
+++ day20/swiftUIStacks/swiftUIStacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIStacks
+//
+//  Created by Choi SeungHyuk on 2020/07/10.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
