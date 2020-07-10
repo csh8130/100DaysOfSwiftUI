b80c079e95f1274a881e854639a67e4cde29ea90
csh8130 <csh8130@naver.com>
2020-07-11 00:21:33 +0900

    swift ui stack 사용법

--- day20/swiftUIStacks/swiftUIStacks/ContentView.swift
+++ day20/swiftUIStacks/swiftUIStacks/ContentView.swift
@@ -10,7 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Hello, World!")
+            Text("This is another text view")
+            HStack(spacing: 20) {
+                Text("Hello World")
+                Text("This is inside a stack")
+            }
+            ZStack {
+                Text("1235678")
+                Text("54326")
+            }
+            VStack {
+                Text("First")
+                Text("Second")
+                Text("Third")
+                Spacer()
+            }
+        }
     }
 }
 
