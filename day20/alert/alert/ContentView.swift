48a1456cb7ad3ac9aec1eff7e064d563e5ebaa7f
csh8130 <csh8130@naver.com>
2020-07-11 01:09:00 +0900

    alert init

---
+++ day20/alert/alert/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  alert
+//
+//  Created by Choi SeungHyuk on 2020/07/11.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingAlert = false
+
+    var body: some View {
+        Button("Show Alert") {
+            self.showingAlert = true
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
