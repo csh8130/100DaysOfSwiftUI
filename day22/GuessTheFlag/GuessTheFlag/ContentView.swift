0fa56ca2199ace3e5aa413db16aebed7655c9d51
csh8130 <csh8130@naver.com>
2020-07-13 23:44:49 +0900

    챌린지 - 스코어 기능 추가

--- day22/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ day22/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score: Int = 0
     
     var body: some View {
         ZStack {
@@ -45,7 +46,7 @@ struct ContentView: View {
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
         }
@@ -54,8 +55,10 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
             scoreTitle = "Wrong"
+            score -= 1
         }
         
         showingScore = true
