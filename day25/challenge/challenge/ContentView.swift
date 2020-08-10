4b6a2914e42fbc33d94e894c4f929847dd307a7c
csh8130 <csh8130@naver.com>
2020-08-10 23:59:17 +0900

    점수 계산 로직

--- day25/challenge/challenge/ContentView.swift
+++ day25/challenge/challenge/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     let states = ["Rock", "Paper", "Scissors"]
     @State var isWin = false
     @State var stateIndex = 0
+    @State var score = 0
     
     private func reset() {
         isWin = Bool.random()
@@ -25,6 +26,26 @@ struct ContentView: View {
             Button("reset") {
                 self.reset()
             }
+            HStack{
+                ForEach(0 ..< states.count) { i in
+                    Button(self.states[i]) {
+                        if self.isWin {
+                            if (i + 1) % 3 == self.stateIndex {
+                                self.score += 1
+                            } else {
+                                self.score -= 1
+                            }
+                        } else {
+                            if (i + 1) % 3 != self.stateIndex {
+                                self.score += 1
+                            } else {
+                                self.score -= 1
+                            }
+                        }
+                    }
+                }
+            }
+            Text("score:\(score)")
         }
         
 //        Text($stateIndex)
