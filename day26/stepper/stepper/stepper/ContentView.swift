9eb2266fb070e3eb5475bdfbe6a46fabb9eff74e
csh8130 <csh8130@naver.com>
2020-08-19 23:57:18 +0900

    Stepper 예제

--- day26/stepper/stepper/stepper/ContentView.swift
+++ day26/stepper/stepper/stepper/ContentView.swift
@@ -9,8 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
-        Text("Hello, World!")
+//        Stepper(value: $sleepAmount) {
+//            Text("\(sleepAmount) hours")
+//        }
+        
+        Stepper(value: $sleepAmount, in: 4...12) {
+            Text("\(sleepAmount) hours")
+        } // in: 을 사용하셔 stepper의 범위를 제한
+        
+//        Text("\(sleepAmount, specifier: "%.2f") hours")
+        // 출력시에 소숫점 두자리까지로 출력을 제한
+        // %g 를 사용할경우 필요없는 0을 자동 제거
     }
 }
 
